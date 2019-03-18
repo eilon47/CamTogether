2ecb4bd731133a2c17bfa539ce56f8ccd63380e7
eilon47 <33837343+eilon47@users.noreply.github.com>
2019-03-18 23:42:29 +0200

    Update image_analysis.py

--- CVserver/image_analysis.py
+++ CVserver/image_analysis.py
@@ -1,43 +1,56 @@
+import io
+
+from PIL import Image
+import socket
 import sys
+
+from PIL.JpegImagePlugin import JpegImageFile
+
 import face_recogize as fr
 import blur_detection as bd
 import cv2 as cv2
-
-
-def main(im):
-	grade = []
-	# open a new image and show it
-	image = cv2.imread(im, 1)
-	highet, width = cv_size(image)
-	total_size = highet*width
-			# i1 = cv2.line(i1, (0,0), (255,255), (0,0,255), 2)
-			# i1 = cv2.rectangle(i1,(33,33), (90,90), (255,0,0), 3)
-			# i1 = cv2.putText(i1, "we are writing on the image!!", (0,50), cv2.FONT_HERSHEY_TRIPLEX, 1, (0,255,0), 10, cv2.LINE_AA)
-			# cv2.imshow('bomba', i1)
-			# k = cv2.waitKey(0)
-			# if k == 27:
-			# 	cv2.destroyAllWindows()
-			# elif k == ord('s'):
-			# 	cv2.imwrite('1_1.png',i1)
-			# 	cv2.destroyAllWindows()
-	is_blur, down_grade = bd.blur_detection(image, total_size, 1000)
-	if is_blur:
-		grade.append(down_grade)
-	else:
-		grade.append(1)
-
-	locations, number_of_faces = fr.number_of_faces(image)
-	if number_of_faces is 0:
-		# check if its landscape
-		pass
-	else:
-		fr.draw_squares_on_image(image, locations)
-	return grade
+import config
+import numpy as np
 
 
 def cv_size(img):
-	return tuple(img.shape[1::-1])
+    return tuple(img.shape[1::-1])
 
 
-if __name__ == '__main__':
-	main(sys.argv[1])
+def handle(client_sock, address):
+    request = client_sock.recv()
+    print('Received {}'.format(len(request)) + " bytes")
+    res = analysis(request)
+    client_sock.send(res)
+    client_sock.close()
+
+
+def analysis(img):
+    nparr = np.fromstring(img, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_LOAD_GDAL)
+    is_blur, down_grade = bd.blur_detection(img, cv_size(img), 1000)
+    if is_blur:
+        return -1
+    locations, num_faces = fr.number_of_faces(img)
+    if num_faces == 1:
+        return -1
+    return 0
+
+
+class Server:
+    def __init__(self):
+        self._port = config.PORT
+        self._ip = config.IP_ADDRESS
+        self.server = None
+
+    def create_connection(self):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind((self._ip, self._port))
+
+    def start(self):
+        while True:
+            self.server.listen(1)
+            client_sock, address = self.server.accept()
+            handle(client_sock, address)
+
+
