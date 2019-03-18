2a7c57782b32cf3be109575cb64d51817dc9b344
eilon47 <33837343+eilon47@users.noreply.github.com>
2019-03-18 23:42:03 +0200

    Update client.py

--- CVserver/CVServer/client.py
+++ CVserver/CVServer/client.py
@@ -1,60 +1,20 @@
-import socket, select
-import cv2
-from configurations import config
+import io
+import socket
 
-TCP_IP = config.IP_ADDRESS
-TCP_PORT = config.PORT
-
+import config
+from PIL import Image
 
 class Client:
-
-    def __init__(self):
-        self.socket = self.connect()
-
-    def connect(self):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_address = (TCP_IP, TCP_PORT)
-        sock.connect(server_address)
-        return sock
-
-    def terminate_connection(self):
-        self.socket.close()
-
-    def send_image(self, image):
-
-        try:
-            # open image
-            img = open('/home/dandan/PycharmProjects/untitled/images/2.jpg', 'rb')
-            bytes = img.read()
-            size = len(bytes)
-
-            # send image size to server
-            self.socket.sendall(b"SIZE %s")
-            answer = self.socket.recv(4096)
-
-            print('answer = %s' % answer)
-
-            # send image to server
-            if answer == 'GOT SIZE':
-                self.socket.sendall(bytes)
-
-                # check what server send
-                answer = self.socket.recv(4096)
-                print('answer = %s' % answer)
-
-                if answer == 'GOT IMAGE' :
-                    self.socket.sendall("BYE BYE ")
-                    print('Image successfully send to server')
-
-        finally:
-            img.close()
-
-def main():
-    image = cv2.imread('/home/dandan/PycharmProjects/untitled/images/2.jpg', 1)
-    client = Client()
-    client.send_image(image)
-    client.terminate_connection()
+    @staticmethod
+    def do():
+        clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        clientsocket.connect((config.IP_ADDRESS, config.PORT))
+        fd = Image.open("/home/eilon/Desktop/CamTogether-master/CVserver/images/1.jpg")
+        imgByteArr = io.BytesIO()
+        fd.save(imgByteArr, format='JPEG')
+        clientsocket.send(imgByteArr.getvalue())
+        ret = clientsocket.recv(1)
+        print("client received " + ret)
 
 if __name__ == '__main__':
-    main()
-
+    Client.do()
