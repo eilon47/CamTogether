bc6da21cf9ae53f612c61d0ebf79a9130797bf84
elon bashari <elon.bashari@live.biu.ac.il>
2019-05-04 17:35:06 +0300

    some changes

--- Server/src/main/java/handlers/AddUserToAlbumHandler.java
+++ Server/src/main/java/handlers/AddUserToAlbumHandler.java
@@ -15,11 +15,8 @@ public class AddUserToAlbumHandler extends CommandHandler {
         ResponseMessage responseMessage = new ResponseMessage();
         responseMessage.setHeader(createHeaderResponse(request.getHeader()));
         AddUserToAlbumRequestBody addUserToAlbumRequestBody = fromXmlToClass(request.getBody(), AddUserToAlbumRequestBody.class);
-        User usrname1 = new User();
-        usrname1.setUserName("username1");
         try {
-            // boolean user_exists = checkUserExistsAndUnique(addUserToAlbumRequestBody.getUserToAdd());
-            boolean user_exists = checkUserExistsAndUnique(usrname1);
+            boolean user_exists = checkUserExistsAndUnique(addUserToAlbumRequestBody.getUserToAdd());
 
             if (!user_exists) {
                 logger.warn("User is not exists, can't add it to album");
@@ -27,9 +24,7 @@ public class AddUserToAlbumHandler extends CommandHandler {
                 responseMessage.setBody("Failed to find user");
                 return responseMessage;
             }
-            // String new_participants_val = createNewParticipantsValue(addUserToAlbumRequestBody.getUserToAdd(), addUserToAlbumRequestBody.getAddToAlbum());
-            String new_participants_val = createNewParticipantsValue(usrname1, addUserToAlbumRequestBody.getAddToAlbum());
-
+            String new_participants_val = createNewParticipantsValue(addUserToAlbumRequestBody.getUserToAdd(), addUserToAlbumRequestBody.getAddToAlbum());
             boolean res = updateNewValue(new_participants_val, addUserToAlbumRequestBody.getAddToAlbum());
             if (!res) {
                 logger.warn("Failed updating user in album's participants");
@@ -38,7 +33,7 @@ public class AddUserToAlbumHandler extends CommandHandler {
                 return responseMessage;
             }
         } catch (ClassNotFoundException | SQLException ex) {
-            logger.error("Failed adding user " + usrname1.getUserName() + " to album", ex);
+            logger.error("Failed adding user " + addUserToAlbumRequestBody.getUserToAdd() + " to album", ex);
             responseMessage.getHeader().setCommandSuccess(false);
             responseMessage.setBody("Request failed");
             return responseMessage;
@@ -47,24 +42,24 @@ public class AddUserToAlbumHandler extends CommandHandler {
         return responseMessage;
     }
 
-    private boolean checkUserExistsAndUnique(User user) throws SQLException, ClassNotFoundException {
+    private boolean checkUserExistsAndUnique(String user) throws SQLException, ClassNotFoundException {
         //Check the User exists in the users table
-        logger.debug("Checking if user " + user.getUserName() + " exists");
+        logger.debug("Checking if user " + user + " exists");
         dbClient.createConnection();
         logger.debug("Connection to db was created");
-        String[] is_user_exist_values = {"", user.getUserName()};
+        String[] is_user_exist_values = {"", user};
         ResultSet resultSet = dbClient.prepareStatementAllStrings(SqlStatements.SELECT_USER_FROM_USERS, is_user_exist_values);
         boolean has_user = resultSet.next();
         boolean has_more_users = resultSet.next(); //Checks we have only 1 user
         if (has_more_users)
             throw new SQLException("Expected to get only one user! many us");
-        logger.info("User " + user.getUserName() + " " + user.getUserName() + " exists result=" + has_user);
+        logger.info("User " + user + " " + user + " exists result=" + has_user);
         resultSet.close();
         dbClient.closeConnection();
         return has_user;
     }
 
-    public String createNewParticipantsValue(User user, String album_name) throws SQLException, ClassNotFoundException {
+    public String createNewParticipantsValue(String user, String album_name) throws SQLException, ClassNotFoundException {
         dbClient.createConnection();
         logger.debug("connection with db created - executing select query to receive participants list");
         ResultSet resultSet = dbClient.selectQuery("participants", "albums", "album_name='" + album_name + "'");
@@ -79,14 +74,14 @@ public class AddUserToAlbumHandler extends CommandHandler {
         resultSet.close();
         dbClient.closeConnection();
         if (participants.isEmpty()) {
-            logger.debug("participants list was empty, adding user " + user.getUserName());
-            return user.getUserName();
+            logger.debug("participants list was empty, adding user " + user);
+            return user;
         }
-        if (participants.contains(user.getUserName())) {
-            logger.debug("participants list already contained user " + user.getUserName());
+        if (participants.contains(user)) {
+            logger.debug("participants list already contained user " + user);
             return participants;
         }
-        participants = participants + "," + user.getUserName();
+        participants = participants + "," + user;
         return participants;
     }
 
