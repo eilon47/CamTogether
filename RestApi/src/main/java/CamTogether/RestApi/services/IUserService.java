85f3e8fefd83979b6d13832d4669d8b38f73bf36
elon bashari <elon.bashari@live.biu.ac.il>
2019-05-08 12:50:16 +0300

    some changes

--- RestApi/src/main/java/CamTogether/RestApi/services/IUserService.java
+++ RestApi/src/main/java/CamTogether/RestApi/services/IUserService.java
@@ -1,10 +1,13 @@
 package CamTogether.RestApi.services;
 
+import xmls.RequestHeader;
 import xmls.User;
 
 public interface IUserService {
 
-    String registerNewUser(User user);
+    String registerNewUser(RequestHeader header, User user);
 
-    String loginUser(String userName, String password);
+    String loginUser(RequestHeader header, String userName, String password);
+
+    String updateUserProfile(RequestHeader header,User user);
 }
--- RestApi/src/main/java/CamTogether/RestApi/services/UserService.java
+++ RestApi/src/main/java/CamTogether/RestApi/services/UserService.java
@@ -1,18 +1,49 @@
 package CamTogether.RestApi.services;
 
 import org.springframework.stereotype.Service;
-import xmls.User;
+import xmls.CommandsEnum;
+import xmls.*;
 
 @Service
 public class UserService extends AbstractService implements IUserService {
+
     @Override
-    public String registerNewUser(User user) {
-        return "Not Implemented Yet!";
+    public String registerNewUser(RequestHeader header, User user) {
+        NewUserRequestBody requestBody = new NewUserRequestBody();
+        requestBody.setUser(user);
+        RequestMessage message = new RequestMessage();
+        message.setHeader(header);
+        ResponseMessage responseMessage = messageToServerAndResponse(message, requestBody);
+        if(responseMessage.getHeader().isCommandSuccess()){
+            return "Success!";
+        }
+        return responseMessage.getHeader().getReason();
     }
 
     @Override
-    public String loginUser(String userName, String password) {
-        return "Login with userName = " + userName + "and password = " + password;
+    public String loginUser(RequestHeader header,String userName, String password) {
+        LoginRequestBody requestBody = new LoginRequestBody();
+        requestBody.setUsername(userName);
+        requestBody.setPassword(password);
+        RequestMessage message = new RequestMessage();
+        message.setHeader(header);
+        ResponseMessage responseMessage = messageToServerAndResponse(message, requestBody);
+        if (responseMessage.getHeader().isCommandSuccess()){
+            return "Success!";
+        }
+        return responseMessage.getHeader().getReason();
+    }
+
+    public String updateUserProfile(RequestHeader header,User user){
+        UpdateUserProfileRequestBody requestBody = new UpdateUserProfileRequestBody();
+        requestBody.setUser(user);
+        RequestMessage message = new RequestMessage();
+        message.setHeader(header);
+        ResponseMessage responseMessage = messageToServerAndResponse(message, requestBody);
+        if (responseMessage.getHeader().isCommandSuccess()){
+            return "Success!";
+        }
+        return responseMessage.getHeader().getReason();
     }
 
 }
